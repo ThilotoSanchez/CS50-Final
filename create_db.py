36d826dbb8dbcf7bc4012e6990a407c2b0e2fef6
ThilotoSanchez <thilo.schinke@gmail.com>
2021-07-19 18:19:17 +0200

    database set up

---
+++ create_db.py
@@ -0,0 +1,59 @@
+import sqlite3
+
+def create_new_db():
+    conn = sqlite3.connect('cov19db.sqlite')
+    cur = conn.cursor()
+
+    # set up table
+    
+    
+    cur.executescript('''
+    CREATE TABLE countries (
+        id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        name      TEXT UNIQUE,
+        continent     TEXT,
+        population     INTEGER
+    );
+
+    CREATE TABLE IF NOT EXISTS cases (
+        id    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        country_id     INTEGER,
+        total     INTEGER,
+        new     TEXT,
+        active     INTEGER,
+        critical     INTEGER,
+        recovered     INTEGER,
+        '1M_POP'     TEXT,
+        day     TEXT,
+        time     TEXT
+    );
+
+    CREATE TABLE IF NOT EXISTS tests (
+        id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        country_id     INTEGER,
+        total     INTEGER,
+        '1M_POP'     TEXT,
+        day     TEXT,
+        time     TEXT
+    );
+
+    CREATE TABLE IF NOT EXISTS deaths (
+        id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        country_id     INTEGER,
+        total     INTEGER,
+        new     TEXT,
+        '1M_POP'     TEXT,
+        day     TEXT,
+        time     TIME
+    );
+
+    CREATE TABLE IF NOT EXISTS user (
+        id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        username     TEXT UNIQUE,
+        pw_hash     TEXT
+    )
+    ''')
+
+    conn.commit()
+
+create_new_db()
--- helpers.py
+++ helpers.py
@@ -2,13 +2,20 @@ import requests
 import json
 import os
 from requests.exceptions import HTTPError
+from dotenv import load_dotenv
+
+# ! Delrecated method, do not use
+# ? Questions?
+# TODO: write to-do here
+
+load_dotenv()
 
 def getCountries():
     url = "https://covid-193.p.rapidapi.com/countries"
 
     headers = {
-        'x-rapidapi-key': x-rapidapi-key,
-        'x-rapidapi-host': x-rapidapi-host
+        'x-rapidapi-key': os.getenv('x-rapidapi-key'),
+        'x-rapidapi-host': os.getenv('x-rapidapi-host')
         }
 
     try:
@@ -22,9 +29,32 @@ def getCountries():
         
         #print(jsonResponse)
         for country in jsonResponse['response']:
-        print(country)
+            print(country)
 
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')
     except Exception as err:
         print(f'Other error occurred: {err}')
+
+def statistics():
+    url = "https://covid-193.p.rapidapi.com/statistics"
+
+    # optional country parameter
+    querystring = {"country":"Germany"}
+
+    headers = {
+        'x-rapidapi-key': os.getenv('x-rapidapi-key'),
+        'x-rapidapi-host': os.getenv('x-rapidapi-host')
+        }
+
+    try:
+        response = requests.request("GET", url, headers=headers, params=querystring)
+
+        print(response.text)
+    
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+
+statistics()
